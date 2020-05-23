fd40a1ceb4018a2dac2da7cf762e05b81a78097e
David Valero <davalerov@itc.edu.co>
2020-05-23 15:36:26 -0500

    Se agrega modelo de miembro

--- IEEE_ETITC/miembro/models.py
+++ IEEE_ETITC/miembro/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+from bases.models import ClaseModelo
 # Create your models here.
+class Miembro(AbstractUser, ClaseModelo):
+    fecha_nacimiento = models.DateField(null=True)
+    celular = models.CharField(max_length=15, null=True)
